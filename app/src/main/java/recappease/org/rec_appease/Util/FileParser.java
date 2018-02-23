58d2a0e4b52cb6237166629ba842d53dbbc35209
rramstad <rramstad@purdue.edu>
2018-02-23 13:57:58 -0500

    Internal Storage

--- app/src/main/java/recappease/org/rec_appease/Util/FileParser.java
+++ app/src/main/java/recappease/org/rec_appease/Util/FileParser.java
@@ -1,6 +1,7 @@
 package recappease.org.rec_appease.Util;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -10,6 +11,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Ramstadr6 on 2/18/2018.
@@ -25,27 +29,34 @@ public class FileParser {
         this.context = context;
     }
 
-    public String readGroceryFile() {
+    public ArrayList<FoodItem> readGroceryFile() {
         try {
             String message;
+            ArrayList<FoodItem> groceryList = new ArrayList<>(30);
             FileInputStream fileInputStream = this.context.openFileInput(groceryFileName);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
+            //StringBuffer stringBuffer = new StringBuffer();
             while ((message = bufferedReader.readLine()) != null) {
-                stringBuffer.append(message + "\n");
+                //stringBuffer.append(message);
+                groceryList.add(new FoodItem(message));
             }
-            return stringBuffer.toString();
+            return groceryList;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<FoodItem>(30);
     }
 
-    public void writeGroceryFile(String message) {
+    public void writeGroceryFile(ArrayList<FoodItem> groceryList) {
         try {
+            Iterator<FoodItem> iterator = groceryList.iterator();
+            String message = "";
+            while (iterator.hasNext()) {
+                message = message + iterator.next().name + "\n";
+            }
             FileOutputStream fileOutputStream = this.context.openFileOutput(groceryFileName, context.MODE_PRIVATE);
             fileOutputStream.write(message.getBytes());
             fileOutputStream.close();
@@ -56,27 +67,34 @@ public class FileParser {
         }
     }
 
-    public String readInventoryFile() {
+    public ArrayList<FoodItem> readInventoryFile() {
         try {
             String message;
+            ArrayList<FoodItem> inventoryList = new ArrayList<>(30);
             FileInputStream fileInputStream = this.context.openFileInput(inventoryFileName);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
+            //StringBuffer stringBuffer = new StringBuffer();
             while ((message = bufferedReader.readLine()) != null) {
-                stringBuffer.append(message + "\n");
+                //stringBuffer.append(message + "\n");
+                inventoryList.add(new FoodItem(message));
             }
-            return stringBuffer.toString();
+            return inventoryList;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<FoodItem>(30);
     }
 
-    public void writeInventoryFile(String message) {
+    public void writeInventoryFile(ArrayList<FoodItem> inventoryList) {
         try {
+            Iterator<FoodItem> iterator = inventoryList.iterator();
+            String message = "";
+            while (iterator.hasNext()) {
+                message = message + iterator.next().name + "\n";
+            }
             FileOutputStream fileOutputStream = this.context.openFileOutput(inventoryFileName, context.MODE_PRIVATE);
             fileOutputStream.write(message.getBytes());
             fileOutputStream.close();
@@ -85,6 +103,19 @@ public class FileParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void checkOutGrocery() {
+        ArrayList<FoodItem> groceryList = readGroceryFile();
+        ArrayList<FoodItem> inventoryList = readInventoryFile();
+        Iterator<FoodItem> iterator = groceryList.iterator();
+        String message = "";
+        while (iterator.hasNext()) {
+            inventoryList.add(new FoodItem(iterator.next().name));
+        }
+        groceryList.clear();
+        writeInventoryFile(inventoryList);
+        writeGroceryFile(groceryList);
     }
 
     public String getGroceryFileName() {
---
+++ app/src/main/java/recappease/org/rec_appease/Util/FoodItem.java
@@ -0,0 +1,13 @@
+package recappease.org.rec_appease.Util;
+
+/**
+ * Created by Ramstadr6 on 2/23/2018.
+ */
+
+public class FoodItem {
+    public String name;
+
+    public FoodItem(String name) {
+        this.name = name;
+    }
+}
